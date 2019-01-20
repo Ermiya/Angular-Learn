899c3af9ab99143bd9bed26bb58233e54a94b048
farshid <farshid.saeidi@hotmail.com>
2019-01-20 10:10:26 +0330

    .

--- Angular 6 TopLearn/j7/file.js
+++ Angular 6 TopLearn/j7/file.js
@@ -1,1 +1,20 @@
-var Name = "ali";
+var Name = 'ali';
+function MySum(value) {
+    return value;
+}
+console.log(MySum(234));
+console.log(MySum('ALI'));
+console.log(MySum('Far'));
+var MyGeneric = /** @class */ (function () {
+    function MyGeneric(param) {
+        this.param = param;
+    }
+    return MyGeneric;
+}());
+var m = new MyGeneric('Farrr');
+console.log(m.param);
+function Names(params) {
+    return params;
+}
+console.log(Names(["AAA", "BBB"]));
+console.log(Names([333, 444, 555]));
--- Angular 6 TopLearn/j7/file.ts
+++ Angular 6 TopLearn/j7/file.ts
@@ -1,1 +1,31 @@
-let Name:string="ali";
+let Name: string = 'ali';
+
+function MySum<T>(value: T): T {
+  return value;
+}
+
+console.log(MySum(234));
+
+console.log(MySum('ALI'));
+
+console.log(MySum<string>('Far'));
+
+class MyGeneric<T> {
+  param: T;
+
+  constructor(param: T) {
+    this.param = param;
+  }
+}
+
+var m = new MyGeneric<string>('Farrr');
+
+console.log(m.param);
+
+function Names<T>(params: T[]): T[] {
+  return params;
+}
+
+console.log(Names<string>(["AAA","BBB"]));
+
+console.log(Names<number>([333,444,555]));
