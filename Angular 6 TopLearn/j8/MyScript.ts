f37607780c4dd71941bcd336870ca94a81094759
farshid <farshid.saeidi@hotmail.com>
2019-01-20 11:19:57 +0330

    .

---
+++ Angular 6 TopLearn/j8/MyScript.ts
@@ -0,0 +1,6 @@
+import {ManageUser} from './Services/ManageUser';
+
+
+var manager = new ManageUser;
+
+manager.Register("111","222");
---
+++ Angular 6 TopLearn/j8/Services/ManageUser.ts
@@ -0,0 +1,10 @@
+
+import {IManageUser} from '../Contracts/IManageUser';
+
+export class ManageUser implements IManageUser
+{
+    Register(userName: string, password: string): void {
+        
+    }
+
+}
