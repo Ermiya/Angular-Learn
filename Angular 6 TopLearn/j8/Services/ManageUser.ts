a04ac81b656deaef4020bd1942e52c49c79221f5
farshid <farshid.saeidi@hotmail.com>
2019-01-20 13:55:35 +0330

    .

--- Angular 6 TopLearn/j8/Services/ManageUser.ts
+++ Angular 6 TopLearn/j8/Services/ManageUser.ts
@@ -1,7 +1,7 @@
 
 import {IManageUser} from '../Contracts/IManageUser';
 
-export class ManageUser implements IManageUser
+export default class ManageUser implements IManageUser
 {
     Register(userName: string, password: string): void {
         
