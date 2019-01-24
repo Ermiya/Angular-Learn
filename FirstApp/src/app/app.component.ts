c42a5262739541dd8d5e2453ee83cee51edd7671
farshid <farshid.saeidi@hotmail.com>
2019-01-24 13:52:25 +0330

    end 19 revert

--- FirstApp/src/app/app.component.ts
+++ FirstApp/src/app/app.component.ts
@@ -1,20 +1,19 @@
-import {Component, Input} from '@angular/core';
-import {Register} from './models/Register';
-
+import { Component, Input } from '@angular/core';
+import { Register } from './models/Register';
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms'
 @Component({
   selector: 'my-app',
   templateUrl: 'app/templates/appComponents.html',
-  styles: [
-    `
-      .ng-valid[required],
-      ng-valid.required {
-        border: 1px solid green;
-      }
-      .ng-invalid:not(form)[class~='ng-touched'] {
-        border: 1px solid red;
-      }
-    `,
-  ],
+  // styles: [`
+  //     .ng-valid[required],
+  //     ng-valid.required {
+  //       border: 1px solid green;
+  //     }
+  //     .ng-invalid:not(form)[class~='ng-touched'] {
+  //       border: 1px solid red;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   //  name = 'Angular';
@@ -24,9 +23,18 @@ export class AppComponent {
   //  number: number = 0;
 
   //  myName:string="farshid";
-  register: Register;
-  save(_register: any) {
+  //register: Register;
+  // save(_register: any) {
 
+  // //Driven form
+  // this.register = new Register(_register.value.username, _register.value.password, _register.value.email, _register.value.address);
+  // //
+  // console.log(this.register);
+  //}
+
+  register: Register;
+
+<<<<<<< HEAD
     this.register = new Register(_register.value.username,_register.value.password,_register.value.email,_register.value.address);
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -38,5 +46,35 @@ export class AppComponent {
 //
 >>>>>>> parent of 5faf6a3... End 19 Reactive Form
     console.log(this.register);
+=======
+  username = new FormControl('',[
+    Validators.required,
+    Validators.minLength(4)
+  
+  ]);
+  password = new FormControl('',[
+    Validators.required
+  
+  ]);
+  email = new FormControl('',[
+    Validators.required
+  
+  ]);
+  address = new FormControl('');
+
+  registerForm: FormGroup = this.builder.group({
+    username: this.username,
+    password: this.password,
+    email: this.email,
+    address: this.address
+  })
+
+  constructor(private builder: FormBuilder) {
+
   }
+  save(_register: any) {
+    console.log(this.registerForm.value);
+>>>>>>> parent of 169f7f6... .
+  }
+
 }
