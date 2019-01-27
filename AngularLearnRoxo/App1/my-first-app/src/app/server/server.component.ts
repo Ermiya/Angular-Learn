4e98672ed66eb4e40e23d15624ff2d6513bc7ef9
Farshid <Farshid@FARSHID-PC>
2019-01-27 22:15:40 +0330

    End Roxo 04

--- AngularLearnRoxo/App1/my-first-app/src/app/server/server.component.ts
+++ AngularLearnRoxo/App1/my-first-app/src/app/server/server.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
-   templateUrl: './server.component.html',
+    selector: 'app-server',
+    templateUrl: './server.component.html',
 
-  styleUrls: ['./server.component.css']
+    styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-  title = 'فرشيد';
-  name='';
+    title = 'فرشيد';
+    name = '';
+
+    id: number = 10;
+    status: string = 'فعال';
+    allowButton: boolean = false;
+    serverCreatedText: string = "هنوز سروری ایجاد نشده است";
+    serverName: string;
+
+    constructor() {
+        setTimeout(() => {
+            this.allowButton = true;
+        }, 3000);
+    }
+    getStatus(): string {
+        return this.status;
+    }
+    ServerCreated() {
+        this.serverCreatedText = "سرور"  +  this.serverName + "با موفقیت ایجاد شد!"
+    }
+    ServerTextRefresh() {
+        this.serverCreatedText = " سروری ریفرش شد";
+    }
+
+    onUpdateServerName(evevnt: any[]) {
+        this.serverName=event.target.value;
+    }
+
+
 }
