2fd28fdf9fff661f1b63836399a1686b7459bdc5
farshid <farshid.saeidi@hotmail.com>
2019-01-30 17:08:32 +0330

    End Roxo 06

--- AngularLearnRoxo/shop/src/app/recpies/recpie-list/recpie-list.component.ts
+++ AngularLearnRoxo/shop/src/app/recpies/recpie-list/recpie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../shared/models/recipe.model';
 
 @Component({
   selector: 'app-recpie-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recpie-list.component.css']
 })
 export class RecpieListComponent implements OnInit {
+recipes: Recipe[]=[
+  new Recipe('دستور پخت ماكاروني','توضيح براي دستور پخت ماكاروني قرار ميدهيم','https://cdn.lifesambrosia.com/wp-content/uploads/spaghetti-with-creamy-marinara.jpg'),
+  new Recipe('دستور پخت ماكاروني','توضيح براي دستور پخت ماكاروني قرار ميدهيم','https://cdn.lifesambrosia.com/wp-content/uploads/spaghetti-with-creamy-marinara.jpg'),
+  new Recipe('دستور پخت ماكاروني','توضيح براي دستور پخت ماكاروني قرار ميدهيم','https://cdn.lifesambrosia.com/wp-content/uploads/spaghetti-with-creamy-marinara.jpg'),
+]
+  constructor() {
 
-  constructor() { }
+   }
 
   ngOnInit() {
   }
--- AngularLearnRoxo/shop/src/app/shopping-list/shopping-list.component.ts
+++ AngularLearnRoxo/shop/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients: Ingredient[] = [
+  new Ingredient('بسته ماكاروني','1'),
+  new Ingredient('پياز', '21'),
+  new Ingredient('سس', '1')
+]
   constructor() { }
 
   ngOnInit() {
