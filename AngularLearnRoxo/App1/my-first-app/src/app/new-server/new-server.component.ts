341c299aa449ab7b971160b2c5570f3eac9b3aa2
Farshid <Farshid@FARSHID-PC>
2019-01-31 23:54:05 +0330

    End Roxo 09

--- AngularLearnRoxo/App1/my-first-app/src/app/new-server/new-server.component.ts
+++ AngularLearnRoxo/App1/my-first-app/src/app/new-server/new-server.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
 
 
 @Component({
   selector: 'app-new-server',
   templateUrl: './new-server.component.html',
-  styleUrls: ['./new-server.component.css']
+  styleUrls: ['./new-server.component.css'],
+  //encapsulation: ViewEncapsulation.Emulated
 })
 export class NewServerComponent implements OnInit {
 
   @Output() iranianServer: EventEmitter<{ nameServer: string, contentServer: string }> = new EventEmitter<{ nameServer: string, contentServer: string }>();
   @Output() germanyServer: EventEmitter<{ nameServer: string, contentServer: string }> = new EventEmitter<{ nameServer: string, contentServer: string }>();;
 
+  @ViewChild('serverContent') serverContent:ElementRef;
 
 
   NewServerName = '';
@@ -23,6 +25,8 @@ export class NewServerComponent implements OnInit {
 
   onAddIranianServer() {
     this.iranianServer.emit({ nameServer: this.NewServerName,contentServer:this.NewServerContent });
+   //this.iranianServer.emit({ nameServer: this.NewServerName,contentServer:this.serverContent.nativeElement.value });
+  //  console.log(this.serverContent.nativeElement.value);
     // this.serverElements.push({
     //   type: 'iran',
     //   name: this.NewServerName,
