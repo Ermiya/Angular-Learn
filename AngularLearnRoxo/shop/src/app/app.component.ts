316d79c0b06666520e78b1385438f8a4e17466da
farshid <farshid.saeidi@hotmail.com>
2019-02-03 14:57:05 +0330

    Roxo 11 15:00

--- AngularLearnRoxo/shop/src/app/app.component.ts
+++ AngularLearnRoxo/shop/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'shop';
+  recipeShow: boolean;
+  constructor() {}
+  onNavigation(event) {
+    if (event.value === 'recipe') {
+      this.recipeShow = true;
+    } else {
+      this.recipeShow = false;
+    }
+  }
 }
--- AngularLearnRoxo/shop/src/app/header/header.component.ts
+++ AngularLearnRoxo/shop/src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() recipeLoaded: EventEmitter<string> = new EventEmitter<string>();
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onSelect(value) {
+    if (value === 'recipe') {
+      this.recipeLoaded.emit('recipe');
+    } else {
+      this.recipeLoaded.emit('shopping-list');
+    }
   }
-
 }
