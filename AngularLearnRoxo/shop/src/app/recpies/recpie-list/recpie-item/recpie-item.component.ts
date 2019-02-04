800803304dfa389f3f006591a637366dcf8842ff
farshid <farshid.saeidi@hotmail.com>
2019-02-04 15:20:55 +0330

    Roxo 11 25:40

--- AngularLearnRoxo/shop/src/app/recpies/recpie-list/recpie-item/recpie-item.component.ts
+++ AngularLearnRoxo/shop/src/app/recpies/recpie-list/recpie-item/recpie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from 'src/app/shared/models/recipe.model';
 
 @Component({
   selector: 'app-recpie-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recpie-item.component.css']
 })
 export class RecpieItemComponent implements OnInit {
-
+@Input()  recipe: Recipe;
   constructor() { }
 
   ngOnInit() {
